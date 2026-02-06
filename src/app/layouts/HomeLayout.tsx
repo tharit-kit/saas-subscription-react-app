8a00522caec5131b68aabba843dc5a3e6758c0f5
Tharit Kitumnuaypong <12055@tecalliance.net>
2026-02-06 21:23:56 +0700

    update code

--- src/app/layouts/HomeLayout.tsx
+++ src/app/layouts/HomeLayout.tsx
@@ -1,9 +1,58 @@
-import { Card } from "primereact/card";
+import { Menubar } from "primereact/menubar";    
+import type { MenuItem } from "primereact/menuitem";
+import { Outlet } from "react-router";
 
 export default function HomeLayout() {
+    const items: MenuItem[] = [
+        {
+            label: 'Home',
+            icon: 'pi pi-home'
+        },
+        {
+            label: 'Features',
+            icon: 'pi pi-star'
+        },
+        {
+            label: 'Projects',
+            icon: 'pi pi-search',
+            items: [
+                {
+                    label: 'Components',
+                    icon: 'pi pi-bolt'
+                },
+                {
+                    label: 'Blocks',
+                    icon: 'pi pi-server'
+                },
+                {
+                    label: 'UI Kit',
+                    icon: 'pi pi-pencil'
+                },
+                {
+                    label: 'Templates',
+                    icon: 'pi pi-palette',
+                    items: [
+                        {
+                            label: 'Apollo',
+                            icon: 'pi pi-palette'
+                        },
+                        {
+                            label: 'Ultima',
+                            icon: 'pi pi-palette'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            label: 'Contact',
+            icon: 'pi pi-envelope'
+        }
+    ];
     return (
-        <Card>
-            
-        </Card>
-    )
+        <div>
+            <Menubar model={items} />
+            <Outlet />
+        </div>
+    );
 }
--- src/features/home/pages/HomePage.tsx
+++ src/features/home/pages/HomePage.tsx
@@ -1,5 +1,42 @@
+import React from 'react'; 
+import { Card } from "primereact/card";
+import { Button } from "primereact/button" 
+
 export default function HomePage(){
+    const header = (
+        <img alt="Card" src="https://primefaces.org/cdn/primereact/images/usercard.png" />
+    );
+    const footer = (
+        <>
+            <Button label="Save" icon="pi pi-check" />
+            <Button label="Cancel" severity="secondary" icon="pi pi-times" style={{ marginLeft: '0.5em' }} />
+        </>
+    );
+
     return(
-        
+        <Card>
+            <div className="flex flex-wrap gap-4 justify-center">
+                <Card title="Advanced Card" subTitle="Card subtitle" footer={footer} header={header} className="w-80">
+                    <p className="m-0">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
+                        numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+                    </p>
+                </Card>
+
+                <Card title="Advanced Card" subTitle="Card subtitle" footer={footer} header={header} className="w-80">
+                    <p className="m-0">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
+                        numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+                    </p>
+                </Card>
+
+                <Card title="Advanced Card" subTitle="Card subtitle" footer={footer} header={header} className="w-80">
+                    <p className="m-0">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
+                        numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+                    </p>
+                </Card>
+            </div>
+        </Card>
     );
 }
