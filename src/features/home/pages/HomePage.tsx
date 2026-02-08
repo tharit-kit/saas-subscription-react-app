fba664dfa0a18de5410693a61d55ce6ec74dfc2a
Tharit Kitumnuaypong <tharit@tt-ss.net>
2026-02-08 23:58:07 +0700

    added home page

--- src/features/home/pages/HomePage.tsx
+++ src/features/home/pages/HomePage.tsx
@@ -33,32 +33,42 @@ export default function HomePage(){
 
     return(
         <Card>
-            <div className="flex flex-wrap gap-4 justify-center">
-                <Card title="Starter" subTitle="Benefits:" footer={footerStarterTier} header={headerStarterTier} className="w-80">
-                    <p className="m-0">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
-                        numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
-                    </p>
-                </Card>
+            <div className="flex flex-col space-y-12">
+                <div className="flex flex-wrap gap-4 justify-center">
+                    <Card title="Starter" subTitle="Benefits:" footer={footerStarterTier} header={headerStarterTier} className="w-80">
+                        <p className="m-0">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
+                            numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+                        </p>
+                    </Card>
 
-                <Card title="Pro" subTitle="Benefits:" footer={footerProTier} header={headerProTier} className="w-80">
-                    <p className="m-0">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
-                        numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
-                    </p>
-                </Card>
+                    <Card title="Pro" subTitle="Benefits:" footer={footerProTier} header={headerProTier} className="w-80">
+                        <p className="m-0">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
+                            numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+                        </p>
+                    </Card>
 
-                <Card title="Enterprise" subTitle="Benefits:" footer={footerEnterpriseTier} header={headerEnterpriseTier} className="w-80">
-                    <p className="m-0">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
-                        numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
-                    </p>
-                </Card>
-            </div>
-            <Divider />
-            <p className="text-5xl">Who use our product</p>
-            <div className="flex flex-wrap gap-4 justify-center">
-                <Image src="/assets/images/logos/amazon_"></Image>
+                    <Card title="Enterprise" subTitle="Benefits:" footer={footerEnterpriseTier} header={headerEnterpriseTier} className="w-80">
+                        <p className="m-0">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae 
+                            numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+                        </p>
+                    </Card>
+                </div>
+                <Divider />
+                <div className="flex flex-col items-center space-y-12">
+                    <p className="text-5xl">Who use our product</p>
+                    <div className="flex flex-wrap gap-6 justify-center">
+                        <Image src="/src/assets/images/logos/amazon_logo.png" width="100" />
+                        <Image src="/src/assets/images/logos/google_logo.png" width="200" />
+                        <Image src="/src/assets/images/logos/instagram_logo.png" width="100" />
+                        <Image src="/src/assets/images/logos/meta_logo.png" width="200" />
+                        <Image src="/src/assets/images/logos/ms_logo.png" width="300" />
+                    </div>
+                </div>
+                <Divider></Divider>
+                
             </div>
         </Card>
     );
